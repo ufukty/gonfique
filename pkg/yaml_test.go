b99cec69ac1584b8742d618a8cab136d7fdd33e6
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-02-01 23:54:15 +0300

    simplifies test TestCreatationAndDecodingInto

--- pkg/yaml_test.go
+++ pkg/yaml_test.go
@@ -62,42 +62,40 @@ func TestCreatationAndDecodingInto(t *testing.T) {
 				t.Fatal(fmt.Errorf("resolving the type spec needed: %w", err))
 			}
 
-			t.Run("writing", func(t *testing.T) {
-				testloc, err := os.MkdirTemp(os.TempDir(), "*")
-				if err != nil {
-					t.Fatal(fmt.Errorf("creating temporary directory to test the created schema: %w", err))
-				}
-				fmt.Println("using tmp dir:", testloc)
+			testloc, err := os.MkdirTemp(os.TempDir(), "*")
+			if err != nil {
+				t.Fatal(fmt.Errorf("creating temporary directory to test the created schema: %w", err))
+			}
+			fmt.Println("using tmp dir:", testloc)
 
-				files := []string{"go.mod", "go.sum", "config_test.go", "config.yml"}
-				for _, file := range files {
-					src := filepath.Join("testdata", tc, file)
-					dst := filepath.Join(testloc, file)
-					if err := copyFile(src, dst); err != nil {
-						t.Fatal(fmt.Errorf("copying %q to %q: %w", file, dst, err))
-					}
+			files := []string{"go.mod", "go.sum", "config_test.go", "config.yml"}
+			for _, file := range files {
+				src := filepath.Join("testdata", tc, file)
+				dst := filepath.Join(testloc, file)
+				if err := copyFile(src, dst); err != nil {
+					t.Fatal(fmt.Errorf("copying %q to %q: %w", file, dst, err))
 				}
+			}
 
-				if err := WriteConfigGo(filepath.Join(testloc, "config.go"), cts, "config"); err != nil {
-					t.Fatal(fmt.Errorf("creating config.go file: %w", err))
-				}
+			if err := WriteConfigGo(filepath.Join(testloc, "config.go"), cts, "config"); err != nil {
+				t.Fatal(fmt.Errorf("creating config.go file: %w", err))
+			}
 
-				t.Run("decode", func(t *testing.T) {
-					cmd := exec.Command("/usr/local/go/bin/go", "test",
-						"-timeout", "10s",
-						"-run", "^TestConfig$",
-						"test",
-						"-v", "-count=1",
-					)
-					cmd.Dir = testloc
-					cmd.Stdout = os.Stderr
-					cmd.Stderr = os.Stderr
-					err := cmd.Run()
-					if err != nil {
-						t.Fatal(fmt.Errorf("running go-test: %w", err))
-					}
-				})
-			})
+			cmd := exec.Command("/usr/local/go/bin/go", "test",
+				"-timeout", "10s",
+				"-run", "^TestConfig$",
+				"test",
+				"-v", "-count=1",
+			)
+			cmd.Dir = testloc
+			cmd.Stdout = os.Stderr
+			cmd.Stderr = os.Stderr
+			err = cmd.Run()
+			if err != nil {
+				t.Fatal(fmt.Errorf("running go-test: %w", err))
+			}
+
 		})
+
 	}
 }
