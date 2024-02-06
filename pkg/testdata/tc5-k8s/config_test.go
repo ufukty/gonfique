dedf40949ef2076330d32f40361fbfaf60f5b548
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-02-06 10:36:05 +0300

    adds test cases for array access

--- pkg/testdata/tc5-k8s/config_test.go
+++ pkg/testdata/tc5-k8s/config_test.go
@@ -14,4 +14,12 @@ func TestConfig(t *testing.T) {
 	if cfg.Spec.Selector.MatchLabels.App != "my-app" {
 		t.Fatal(fmt.Errorf("expected %q got %q", "my-app", cfg.Spec.Selector.MatchLabels.App))
 	}
+
+	if cfg.Spec.Ports[0].Port != 80 {
+		t.Fatal(fmt.Errorf("array access, expected %q got %q", "80", cfg.Spec.Ports[0].Port))
+	}
+
+	if l := len(cfg.Spec.Template.Spec.Containers[0].EnvFrom); l != 2 {
+		t.Fatal(fmt.Errorf("array lentgh, expected %q got %q", "2", l))
+	}
 }
