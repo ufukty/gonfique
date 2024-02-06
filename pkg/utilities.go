2bffac628614bd01fb6da7fdc10d9f8a6a444cb3
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-02-06 17:02:59 +0300

    renames utiliities.go file, adds bijective26 function for creating unique-effortless names for each separate type specification

--- pkg/utilities.go
+++ pkg/utilities.go
@@ -16,3 +16,15 @@ func sort(idts []*ast.Ident) {
 		}
 	})
 }
+
+func bijective26(i int) string {
+	const q = 26
+	i++
+	s := ""
+	for i > 0 {
+		i--
+		s = string(byte(int(byte('A'))+i%q)) + s
+		i /= q
+	}
+	return s
+}
