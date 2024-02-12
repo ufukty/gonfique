1601fc19ca3cdf657e24539dee4b4be294754802
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-02-12 21:22:47 +0300

    completes Match function, making it pass tests

--- pkg/matcher.go
+++ pkg/matcher.go
@@ -11,70 +11,97 @@ import (
 
 var ErrNoResult = fmt.Errorf("no result")
 
-type candidate struct {
-	i *ast.Ident
-	v reflect.Value // to replace its place in AST
+func degraded(keys []string) []string {
+	keys2 := slices.Clone(keys)
+	keys2[0] = "*" // replaces "**"
+	return keys2
 }
 
-// segms <- keypath segments
-func match(t ast.Expr, keys []string, anc []string) []candidate {
+type node struct {
+	i *ast.Ident
+	v reflect.Value // to let owner able to put another node to its place in AST
+}
+
+func match(n ast.Node, keys []string) []ast.Node {
 	if len(keys) == 0 {
-		return []candidate{}
+		return []ast.Node{}
 	}
-	replaced := []candidate{}
-	switch t := t.(type) {
-	case *ast.StructType:
-		switch key := keys[0]; key {
-		case "**":
-			log.Fatalln("to implement")
-		case "*":
-			for _, f := range t.Fields.List {
-				replaced = append(replaced,
-					match(f, keys[1:], append(slices.Clone(anc), f.Names[0].Name))...,
-				)
+
+	var t ast.Expr
+	switch n := n.(type) {
+	case *ast.TypeSpec:
+		t = n.Type
+	case *ast.Field:
+		t = n.Type
+	case *ast.Ident:
+		log.Fatalln("to implement 2")
+	case *ast.ArrayType:
+		t = n.Elt
+	default:
+		log.Fatalln("unhandled type", reflect.TypeOf(n))
+	}
+
+	fmt.Println(strings.Join(keys, "."))
+
+	matches := []ast.Node{}
+
+	switch key := keys[0]; key {
+	case "**": // works like Levenshtein (DP)
+		if st, ok := t.(*ast.StructType); ok {
+			for _, f := range st.Fields.List {
+				matches = append(matches, match(f, keys)...)
+				matches = append(matches, match(f, degraded(keys))...)
 			}
-		default:
-			for _, f := range t.Fields.List {
-				kn, err := stripKeyname(f.Tag.Value)
+		}
+
+	case "*":
+		if st, ok := t.(*ast.StructType); ok {
+			for _, f := range st.Fields.List {
+				matches = append(matches, match(f, keys[1:])...)
+			}
+		}
+
+	case "[]": // other selectors like [birthday], [photo,email] are reserved for later use
+		if at, ok := t.(*ast.ArrayType); ok {
+			if len(keys) == 1 { // should be leaf
+				matches = append(matches, at.Elt)
+			} else {
+				matches = append(matches, match(at, keys[1:])...)
+			}
+		}
+
+	default:
+		if st, ok := t.(*ast.StructType); ok {
+			for _, f := range st.Fields.List {
+				ckey, err := stripKeyname(f.Tag.Value)
 				if err != nil {
 					log.Fatalf("could not get the key name out of field tag for %s", f.Tag.Value)
 				}
-				if kn == key {
-
+				if ckey == key {
+					if len(keys) == 1 { // should be leaf
+						matches = append(matches, f)
+					} else {
+						matches = append(matches, match(f, keys[1:])...)
+					}
 				}
-				replaced = append(replaced, candidate{}) // f.Type
 			}
-
 		}
-
-	case *ast.ArrayType:
-		if keys[0] != "[*]" {
-
-		}
-		log.Fatalln("to implement")
-
-	default:
-		if len(keys) != 1 { // no match
-			return []candidate{}
-		}
-
 	}
-	return replaced
+	return matches
 }
 
-func MatchAndReplace(cfg *ast.TypeSpec, keypath string) []string {
+// accepts processed form of Config type AST which:
+//   - should not have multiple names per ast.Field
+//   - array types should be defined by combining compatible item fields
+func Match(cfg *ast.TypeSpec, keypath string) []ast.Node {
 	keys := strings.Split(keypath, ".")
 	if len(keys) == 0 {
-		return []string{}
-	} else if l := keys[len(keys)-1]; l == "*" || l == "[*]" {
-		return []string{}
+		return []ast.Node{}
+	} else if l := keys[len(keys)-1]; l == "*" || l == "**" {
+		return []ast.Node{}
 	}
 
-	cds := match(cfg.Type, keys, []string{})
+	cds := match(cfg, keys)
 
-	for _, cd := range cds {
-		fmt.Println(cd)
-	}
-
-	return nil
+	return cds
 }
