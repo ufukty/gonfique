e5c7f451fd732972b1e7b437f4c9dc66516243a6
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-04-10 18:52:22 +0300

    moves one-use functions from generic package

--- pkg/fieldlist/fieldlist.go
+++ pkg/fieldlist/fieldlist.go
@@ -1,21 +1,6 @@
 package fieldlist
 
-import (
-	"go/ast"
-	"slices"
-)
-
-func Sort(fl *ast.FieldList) {
-	slices.SortFunc(fl.List, func(a, b *ast.Field) int {
-		if a.Names[0].Name < b.Names[0].Name {
-			return -1
-		} else if a.Names[0].Name > b.Names[0].Name {
-			return 1
-		} else {
-			return 0
-		}
-	})
-}
+import "go/ast"
 
 // Separates multiple field definitions per Field instance into isolated Field entries
 func Flatten(fl *ast.FieldList) *ast.FieldList {
@@ -30,21 +15,4 @@ func Flatten(fl *ast.FieldList) *ast.FieldList {
 		}
 	}
 	return flttn
-}
-
-// merges 2 FieldList assumed do not contain same field with different types
-func Combine(a, b *ast.FieldList) *ast.FieldList {
-	m := &ast.FieldList{}
-	addedIdents := map[string]bool{}
-	for _, f := range a.List {
-		m.List = append(m.List, f)
-		addedIdents[f.Names[0].Name] = true
-	}
-	for _, f := range b.List {
-		if _, ok := addedIdents[f.Names[0].Name]; !ok {
-			m.List = append(m.List, f)
-			addedIdents[f.Names[0].Name] = true
-		}
-	}
-	return m
 }
