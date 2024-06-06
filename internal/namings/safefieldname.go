33b8f8d48574097e1902cbe10e94d197909143bb
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-06-06 18:21:55 +0300

    adds SafeTypeName to prep upcoming automatic type name assignment based on keypath

--- internal/namings/safefieldname.go
+++ internal/namings/safefieldname.go
@@ -27,3 +27,24 @@ func SafeFieldName(keyname string) string {
 	}
 	return n
 }
+
+func SafeTypeName(keyname string, exported bool) string {
+	if smallcaps.Find([]byte(keyname)) == nil {
+		keyname = strings.ToLower(keyname)
+	}
+	n := ""
+	newSegment := false
+	for i, r := range keyname {
+		if exported && i == 0 {
+			n += strings.ToUpper(string(r))
+		} else if newSegment {
+			n += strings.ToUpper(string(r))
+			newSegment = false
+		} else if !(unicode.IsLetter(r) || unicode.IsNumber(r)) {
+			newSegment = true
+		} else {
+			n += string(r)
+		}
+	}
+	return n
+}
