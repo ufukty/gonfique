46405431b2d73458f1a3daee3817bcd91034995b
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-06-30 15:16:40 +0300

    coder: switches to use pointer values for passing Config instances as call arguments and return value to fix value of parent refs becoming irrelevant after pass

--- internal/coder/asts.go
+++ internal/coder/asts.go
@@ -15,12 +15,10 @@ func addParentRefAssignmentsFunction(b *bundle.Bundle, dst *ast.File) {
 		Type: &ast.FuncType{Params: &ast.FieldList{
 			List: []*ast.Field{{
 				Names: []*ast.Ident{ast.NewIdent("c")},
-				Type:  &ast.Ident{Name: b.TypeName},
+				Type:  &ast.StarExpr{X: ast.NewIdent(b.TypeName)},
 			}},
 		}},
-		Body: &ast.BlockStmt{
-			List: b.ParentRefAssignStmts,
-		},
+		Body: &ast.BlockStmt{List: b.ParentRefAssignStmts},
 	}
 	dst.Decls = append(dst.Decls, fd)
 }
@@ -30,125 +28,62 @@ func addReaderFunction(b *bundle.Bundle, dst *ast.File) {
 	switch b.Encoding {
 	case models.Json:
 		decoder = &ast.SelectorExpr{
-			X: &ast.Ident{
-				Name: "json",
-			},
-			Sel: &ast.Ident{
-				Name: "NewDecoder",
-			},
+			X:   &ast.Ident{Name: "json"},
+			Sel: &ast.Ident{Name: "NewDecoder"},
 		}
 	case models.Yaml:
 		decoder = &ast.SelectorExpr{
-			X: &ast.Ident{
-				Name: "yaml",
-			},
-			Sel: &ast.Ident{
-				Name: "NewDecoder",
-			},
+			X:   &ast.Ident{Name: "yaml"},
+			Sel: &ast.Ident{Name: "NewDecoder"},
 		}
 	}
 
 	readerFunc := &ast.FuncDecl{
-		Name: &ast.Ident{
-			Name: "Read" + b.TypeName,
-		},
+		Name: &ast.Ident{Name: "Read" + b.TypeName},
 		Type: &ast.FuncType{
-			Params: &ast.FieldList{
-				List: []*ast.Field{
-					{
-						Names: []*ast.Ident{
-							{
-								Name: "path",
-							},
-						},
-						Type: &ast.Ident{
-							Name: "string",
-						},
-					},
-				},
-			},
-			Results: &ast.FieldList{
-				List: []*ast.Field{
-					{
-						Type: &ast.Ident{
-							Name: b.TypeName,
-						},
-					},
-					{
-						Type: &ast.Ident{
-							Name: "error",
-						},
-					},
-				},
-			},
+			Params: &ast.FieldList{List: []*ast.Field{{
+				Names: []*ast.Ident{{Name: "path"}},
+				Type:  &ast.Ident{Name: "string"},
+			}}},
+			Results: &ast.FieldList{List: []*ast.Field{
+				{Type: &ast.StarExpr{X: &ast.Ident{Name: b.TypeName}}},
+				{Type: &ast.Ident{Name: "error"}},
+			}},
 		},
 		Body: &ast.BlockStmt{
 			List: []ast.Stmt{
 				&ast.AssignStmt{
-					Lhs: []ast.Expr{
-						&ast.Ident{
-							Name: "file",
-						},
-						&ast.Ident{
-							Name: "err",
-						},
-					},
+					Lhs: []ast.Expr{&ast.Ident{Name: "file"}, &ast.Ident{Name: "err"}},
 					Tok: token.DEFINE,
 					Rhs: []ast.Expr{
 						&ast.CallExpr{
 							Fun: &ast.SelectorExpr{
-								X: &ast.Ident{
-									Name: "os",
-								},
-								Sel: &ast.Ident{
-									Name: "Open",
-								},
+								X:   &ast.Ident{Name: "os"},
+								Sel: &ast.Ident{Name: "Open"},
 							},
-							Args: []ast.Expr{
-								&ast.Ident{
-									Name: "path",
-								},
-							},
+							Args: []ast.Expr{&ast.Ident{Name: "path"}},
 						},
 					},
 				},
 				&ast.IfStmt{
 					Cond: &ast.BinaryExpr{
-						X: &ast.Ident{
-							Name: "err",
-						},
+						X:  &ast.Ident{Name: "err"},
 						Op: token.NEQ,
-						Y: &ast.Ident{
-							Name: "nil",
-						},
+						Y:  &ast.Ident{Name: "nil"},
 					},
 					Body: &ast.BlockStmt{
 						List: []ast.Stmt{
 							&ast.ReturnStmt{
 								Results: []ast.Expr{
-									&ast.CompositeLit{
-										Type: &ast.Ident{
-											Name: b.TypeName,
-										},
-										Incomplete: false,
-									},
+									&ast.Ident{Name: "nil"},
 									&ast.CallExpr{
 										Fun: &ast.SelectorExpr{
-											X: &ast.Ident{
-												Name: "fmt",
-											},
-											Sel: &ast.Ident{
-												Name: "Errorf",
-											},
+											X:   &ast.Ident{Name: "fmt"},
+											Sel: &ast.Ident{Name: "Errorf"},
 										},
 										Args: []ast.Expr{
-											&ast.BasicLit{
-												Kind:  token.STRING,
-												Value: "\"opening config file: %w\"",
-											},
-											&ast.Ident{
-												Name: "err",
-											},
+											&ast.BasicLit{Kind: token.STRING, Value: "\"opening config file: %w\""},
+											&ast.Ident{Name: "err"},
 										},
 									},
 								},
@@ -166,91 +101,53 @@ func addReaderFunction(b *bundle.Bundle, dst *ast.File) {
 					},
 				},
 				&ast.AssignStmt{
-					Lhs: []ast.Expr{
-						&ast.Ident{
-							Name: b.TypeNameInitial,
-						},
-					},
+					Lhs: []ast.Expr{&ast.Ident{Name: b.TypeNameInitial}},
 					Tok: token.DEFINE,
 					Rhs: []ast.Expr{
-						&ast.CompositeLit{
-							Type: &ast.Ident{
-								Name: b.TypeName,
-							},
+						&ast.UnaryExpr{Op: token.AND, X: &ast.CompositeLit{
+							Type:       &ast.Ident{Name: b.TypeName},
 							Incomplete: false,
-						},
+						}},
 					},
 				},
 				&ast.AssignStmt{
-					Lhs: []ast.Expr{
-						&ast.Ident{
-							Name: "err",
-						},
-					},
+					Lhs: []ast.Expr{&ast.Ident{Name: "err"}},
 					Tok: token.ASSIGN,
 					Rhs: []ast.Expr{
 						&ast.CallExpr{
 							Fun: &ast.SelectorExpr{
 								X: &ast.CallExpr{
-									Fun: decoder,
-									Args: []ast.Expr{
-										&ast.Ident{
-											Name: "file",
-										},
-									},
+									Fun:  decoder,
+									Args: []ast.Expr{&ast.Ident{Name: "file"}},
 								},
-								Sel: &ast.Ident{
-									Name: "Decode",
-								},
+								Sel: &ast.Ident{Name: "Decode"},
 							},
-							Args: []ast.Expr{
-								&ast.UnaryExpr{
-									Op: token.AND,
-									X: &ast.Ident{
-										Name: b.TypeNameInitial,
-									},
-								},
-							},
+							Args: []ast.Expr{ast.NewIdent(b.TypeNameInitial)},
 						},
 					},
 				},
 				&ast.IfStmt{
 					Cond: &ast.BinaryExpr{
-						X: &ast.Ident{
-							Name: "err",
-						},
+						X:  &ast.Ident{Name: "err"},
 						Op: token.NEQ,
-						Y: &ast.Ident{
-							Name: "nil",
-						},
+						Y:  &ast.Ident{Name: "nil"},
 					},
 					Body: &ast.BlockStmt{
 						List: []ast.Stmt{
 							&ast.ReturnStmt{
 								Results: []ast.Expr{
-									&ast.CompositeLit{
-										Type: &ast.Ident{
-											Name: b.TypeName,
-										},
-										Incomplete: false,
-									},
+									&ast.Ident{Name: "nil"},
 									&ast.CallExpr{
 										Fun: &ast.SelectorExpr{
-											X: &ast.Ident{
-												Name: "fmt",
-											},
-											Sel: &ast.Ident{
-												Name: "Errorf",
-											},
+											X:   &ast.Ident{Name: "fmt"},
+											Sel: &ast.Ident{Name: "Errorf"},
 										},
 										Args: []ast.Expr{
 											&ast.BasicLit{
 												Kind:  token.STRING,
 												Value: "\"decoding config file: %w\"",
 											},
-											&ast.Ident{
-												Name: "err",
-											},
+											&ast.Ident{Name: "err"},
 										},
 									},
 								},
@@ -260,12 +157,8 @@ func addReaderFunction(b *bundle.Bundle, dst *ast.File) {
 				},
 				&ast.ReturnStmt{
 					Results: []ast.Expr{
-						&ast.Ident{
-							Name: b.TypeNameInitial,
-						},
-						&ast.Ident{
-							Name: "nil",
-						},
+						&ast.Ident{Name: b.TypeNameInitial},
+						&ast.Ident{Name: "nil"},
 					},
 				},
 			},
