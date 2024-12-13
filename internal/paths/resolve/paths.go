64bb8f10cb318bdf590a5d5961328c27a5b7ddb3
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-12-13 21:37:55 +0300

    int/paths/resolve: readability

--- internal/paths/resolve/paths.go
+++ internal/paths/resolve/paths.go
@@ -31,28 +31,28 @@ type resolver struct {
 	paths map[ast.Node]Path
 }
 
-func (r *resolver) dfs(n ast.Node, holder ast.Node, path []string) {
-	if holder != nil {
-		r.paths[holder] = Path(strings.Join(path, "."))
-	}
-
+func (r *resolver) dfs(n ast.Node, path []string) {
 	switch n := n.(type) {
 	case *ast.StructType:
 		if n.Fields != nil && n.Fields.List != nil {
 			for _, f := range n.Fields.List {
 				if f != nil && f.Type != nil {
-					r.dfs(f.Type, f, append(path, r.ti.Keys[f])) // FIXME: for `dict: dynamic`
+					path := append(path, r.ti.Keys[f])
+					r.paths[f] = Path(strings.Join(path, "."))
+					r.dfs(f.Type, path)
 				}
 			}
 		}
 
 	case *ast.ArrayType:
-		r.dfs(n.Elt, n, append(path, "[]"))
+		path := append(path, "[]")
+		r.paths[n] = Path(strings.Join(path, "."))
+		r.dfs(n.Elt, path)
 	}
 }
 
 func Paths(ti *transform.Info) map[ast.Node]Path {
 	r := resolver{ti: ti, paths: map[ast.Node]Path{}}
-	r.dfs(ti.Type, nil, []string{})
+	r.dfs(ti.Type, []string{})
 	return r.paths
 }
