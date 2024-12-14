9be57d5447ef2adcb7e7588204d600b15b5ef99c
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-12-14 22:39:46 +0300

    int/paths/pick: fixes `Values` assign zero values for paths doesn't have a directive set on

--- internal/paths/pick/directives.go
+++ internal/paths/pick/directives.go
@@ -1,44 +1,21 @@
 package pick
 
 import (
-	"maps"
-
 	"github.com/ufukty/gonfique/internal/files/config"
 	"github.com/ufukty/gonfique/internal/paths/resolve"
 )
 
-type valueSources[C comparable] map[C][]config.Path
-
-func (vs valueSources[C]) Add(value C, source config.Path) {
-	if _, ok := vs[value]; !ok {
-		vs[value] = []config.Path{}
-	}
-	vs[value] = append(vs[value], source)
-}
-
-func (vs valueSources[C]) First() C {
-	for k := range maps.Keys(vs) {
-		return k
-	}
-	var zero C
-	return zero
-}
-
-// it returns all values of one directive for each target
+// it returns the directive value for each target
 func Values[C comparable](rev map[resolve.Path][]config.Path, value func(cp config.Path) C) map[resolve.Path]C {
 	var zero C
-	vss := map[resolve.Path]valueSources[C]{}
+	picks := map[resolve.Path]C{}
 	for rp, cps := range rev {
-		vss[rp] = valueSources[C]{}
 		for _, cp := range cps {
 			if v := value(cp); v != zero {
-				vss[rp].Add(v, cp)
+				picks[rp] = v
+				break
 			}
 		}
-	}
-	picks := map[resolve.Path]C{}
-	for rp, vs := range vss {
-		picks[rp] = vs.First()
 	}
 	return picks
 }
