60fb9ac2b7ab0304c32995d274210cb322c54cff
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-12-15 15:47:44 +0300

    int/files/coder: fixes 3rd party and stdlib packages are printed in same island

--- internal/files/coder/sort/sort.go
+++ internal/files/coder/sort/sort.go
@@ -3,13 +3,23 @@ package sort
 import (
 	"go/ast"
 	"slices"
+	"strings"
 )
+
+// does [s] contain a dot in first segment?
+func thirdparty(s string) bool {
+	return strings.Contains(strings.Split(s, "/")[0], ".")
+}
 
 func Imports(specs []ast.Spec) {
 	slices.SortFunc(specs, func(a, b ast.Spec) int {
 		sa := a.(*ast.ImportSpec).Path.Value
 		sb := b.(*ast.ImportSpec).Path.Value
-		if sa < sb {
+		if !thirdparty(sa) && thirdparty(sb) {
+			return -1
+		} else if thirdparty(sa) && !thirdparty(sb) {
+			return 1
+		} else if sa < sb {
 			return -1
 		} else if sa == sb {
 			return 0
