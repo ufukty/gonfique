23537a179ad7d383cc77b87b1638516c6f11a1be
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-12-17 14:02:18 +0300

    int/paths/conflicts: changes it to be called per node in `visitor.visit` of `paths`

--- internal/paths/conflicts/check.go
+++ internal/paths/conflicts/check.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	"github.com/ufukty/gonfique/internal/files/config"
-	"github.com/ufukty/gonfique/internal/paths/resolve"
 )
 
 type valueSources[C comparable] map[C][]config.Path
@@ -29,56 +28,50 @@ func indentlines(s string, n int) string {
 	return indent(n) + strings.ReplaceAll(s, "\n", "\n"+indent(n))
 }
 
+func format[C comparable](vs valueSources[C]) string {
+	msg := ""
+	for value, cps := range vs {
+		msg += fmt.Sprintf("value: %v\n", value)
+		for _, cp := range cps {
+			msg += fmt.Sprintf("%sset by: %s\n", indent(2), cp)
+		}
+	}
+	return msg
+}
+
 // it returns an error of conflicting values on same target set by different paths
 // it uses 'value' argument to get the value from correct directive
-func assertSingleValuePerPath[C comparable](rev map[resolve.Path][]config.Path, value func(cp config.Path) C) error {
+func assertSingleValue[C comparable](cps []config.Path, value func(cp config.Path) C) error {
 	var zero C
-	vss := map[resolve.Path]valueSources[C]{}
-	for rp, cps := range rev {
-		vss[rp] = valueSources[C]{}
-		for _, cp := range cps {
-			if v := value(cp); v != zero {
-				vss[rp].Add(v, cp)
-			}
+	vs := valueSources[C]{}
+	for _, cp := range cps {
+		if v := value(cp); v != zero {
+			vs.Add(v, cp)
 		}
 	}
-
-	msgs := []string{}
-	for rp, vs := range vss {
-		if len(vs) > 1 {
-			msg := fmt.Sprintf("path: %s\n", rp)
-			for value, cps := range vs {
-				msg += fmt.Sprintf("%svalue: %v\n", indent(2), value)
-				for _, cp := range cps {
-					msg += fmt.Sprintf("%sset by: %s\n", indent(4), cp)
-				}
-			}
-			msgs = append(msgs, msg)
-		}
-	}
-
-	if len(msgs) > 0 {
-		return fmt.Errorf(strings.Join(msgs, ""))
+	if len(vs) > 1 {
+		return fmt.Errorf("%s", format(vs))
 	}
 	return nil
 }
 
-func Check(rev map[resolve.Path][]config.Path, c *config.File) error {
+func Check(c map[config.Path]config.PathConfig, cps []config.Path) error {
 	checks := map[string]error{
-		"declare": assertSingleValuePerPath(rev, func(cp config.Path) config.Typename { return c.Paths[cp].Declare }),
-		"export":  assertSingleValuePerPath(rev, func(cp config.Path) bool { return c.Paths[cp].Export }),
-		"dict":    assertSingleValuePerPath(rev, func(cp config.Path) config.Dict { return c.Paths[cp].Dict }),
-		"replace": assertSingleValuePerPath(rev, func(cp config.Path) string { return c.Paths[cp].Replace }),
+		"declare": assertSingleValue(cps, func(cp config.Path) config.Typename { return c[cp].Declare }),
+		"export":  assertSingleValue(cps, func(cp config.Path) bool { return c[cp].Export }),
+		"dict":    assertSingleValue(cps, func(cp config.Path) config.Dict { return c[cp].Dict }),
+		"replace": assertSingleValue(cps, func(cp config.Path) string { return c[cp].Replace }),
 	}
 
 	conflicts := []string{}
 	for directive, err := range checks {
 		if err != nil {
-			conflicts = append(conflicts, fmt.Sprintf("directive: %s\n%s", directive, indentlines(err.Error(), 2)))
+			msg := fmt.Sprintf("directive: %s\n%s", directive, indentlines(err.Error(), 2))
+			conflicts = append(conflicts, msg)
 		}
 	}
 	if len(conflicts) > 0 {
-		return fmt.Errorf(strings.Join(conflicts, "\n"))
+		return fmt.Errorf("%s", strings.Join(conflicts, "\n"))
 	}
 	return nil
 }
