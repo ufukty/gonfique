70f15b71d92e890afa7a4457ed09bd88625f2f86
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-12-17 13:44:34 +0300

    int/paths/pick: changes it to be called per node in `visitor.visit` of `paths`

--- internal/paths/pick/directives.go
+++ internal/paths/pick/directives.go
@@ -2,20 +2,58 @@ package pick
 
 import (
 	"github.com/ufukty/gonfique/internal/files/config"
-	"github.com/ufukty/gonfique/internal/paths/resolve"
 )
 
-// it returns the directive value for each target
-func Values[C comparable](rev map[resolve.Path][]config.Path, value func(cp config.Path) C) map[resolve.Path]C {
+// it returns the first directive value
+// for one target
+func Value[C comparable](paths []config.Path, value func(cp config.Path) C) (C, bool) {
 	var zero C
-	picks := map[resolve.Path]C{}
-	for rp, cps := range rev {
-		for _, cp := range cps {
-			if v := value(cp); v != zero {
-				picks[rp] = v
-				break
-			}
+	for _, cp := range paths {
+		if v := value(cp); v != zero {
+			return v, true
 		}
 	}
-	return picks
+	return zero, false
+}
+
+type set struct {
+	Declare *config.Typename
+	Export  *bool
+	Replace *string
+	Dict    *config.Dict
+}
+
+// it returns the first directive value
+// for every directive
+// for one target
+func Set(paths map[config.Path]config.PathConfig, cps []config.Path) set {
+	s := set{}
+	decl, ok := Value(cps, func(cp config.Path) config.Typename {
+		return paths[cp].Declare
+	})
+	if ok {
+		s.Declare = &decl
+	}
+	expo, ok := Value(cps, func(cp config.Path) bool {
+		return paths[cp].Export && paths[cp].Declare == "" && paths[cp].Replace == ""
+	})
+	if ok {
+		s.Export = &expo
+	}
+	repl, ok := Value(cps, func(cp config.Path) string {
+		return paths[cp].Replace
+	})
+	if ok {
+		s.Replace = &repl
+	}
+	dict, ok := Value(cps, func(cp config.Path) config.Dict {
+		if v := paths[cp].Dict; v != config.Struct {
+			return v
+		}
+		return ""
+	})
+	if ok {
+		s.Dict = &dict
+	}
+	return s
 }
