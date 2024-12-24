b448701014dc2b5793f087d4b66fe8dd17111332
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-12-24 12:44:40 +0300

    int/types: init

---
+++ internal/types/types.go
@@ -0,0 +1,14 @@
+package types
+
+import (
+	"github.com/ufukty/gonfique/internal/files/config"
+	"github.com/ufukty/gonfique/internal/transform"
+)
+
+// TODO: apply parent directive
+// TODO: apply embed directive
+// TODO: implement accessors
+// TODO: implement iterator
+func Apply(ti *transform.Info, c config.File) error {
+	return nil
+}
