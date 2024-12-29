a3bdadf90bb334f17484594ed1b9ec48ee6ff3bb
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-12-29 23:09:05 +0300

    cmd/gonfique/cmds/generate/testdata/api: fixes the test mentioning a map type that is generated by previous config

--- cmd/gonfique/commands/generate/testdata/api/extend.go
+++ cmd/gonfique/commands/generate/testdata/api/extend.go
@@ -5,5 +5,3 @@ import "fmt"
 func (e Endpoint) String() string { // if this doesn't throw error in compilation, test will complete
 	return fmt.Sprintf("%s %s", e.Method, e.Path)
 }
-
-var _ map[string]Endpoint = TagsEndpoints{}
